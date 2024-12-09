aeb60e966c3ce1cf8700a38f0b6345e4cdb5fb75
Huiran Wang <ryan@HuirandeMacBook-Pro.local>
2024-12-09 15:19:30 -0500

    Add GET /api/courses/:courseId/modules route

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,16 +1,21 @@
 import * as modulesDao from "./dao.js";
 export default function ModuleRoutes(app) {
-    app.put("/api/modules/:moduleId", async (req, res) => {
-        const { moduleId } = req.params;
-        const moduleUpdates = req.body;
-        const status = await modulesDao.updateModule(moduleId, moduleUpdates);
-        res.send(status);
-      });
-       
- app.delete("/api/modules/:moduleId", async (req, res) => {
-   const { moduleId } = req.params;
-   const status = await modulesDao.deleteModule(moduleId);
-   res.send(status);
- });
+  app.put("/api/modules/:moduleId", async (req, res) => {
+    const { moduleId } = req.params;
+    const moduleUpdates = req.body;
+    const status = await modulesDao.updateModule(moduleId, moduleUpdates);
+    res.send(status);
+  });
+
+  app.delete("/api/modules/:moduleId", async (req, res) => {
+    const { moduleId } = req.params;
+    const status = await modulesDao.deleteModule(moduleId);
+    res.send(status);
+  });
+  app.get("/api/courses/:courseId/modules", (req, res) => {
+    const { courseId } = req.params;
+    const modules = modulesDao.findModulesForCourse(courseId);
+    res.json(modules);
+  });
 }
 
